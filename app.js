ed1dc7032ba46a953d9461182134541fda66c526
Michael Maysonet <mmaysonet@wovenware.com>
2016-10-16 09:28:59 -0400

    Educating LUCI

--- app.js
+++ app.js
@@ -29,6 +29,17 @@ var presentationIntent = new builder.IntentDialog();
 var finalIntent = new builder.IntentDialog();
 
 var defaultCount = 0;
+var timeOfDay = new Date();
+var greeting;
+
+console.log(timeOfDay.getHours());
+if (timeOfDay.getHours() > 4 && timeOfDay.getHours() < 12) {
+    greeting = "¡Buenos días ";
+} else if (timeOfDay.getHours() >= 12 && timeOfDay.getHours() < 17) {
+     greeting = "¡Buenos tardes ";
+} else {
+     greeting = "¡Buenas noches ";
+}
 
 bot.dialog('/', welcomeIntent);
 bot.dialog('/presention', presentationIntent);
@@ -40,7 +51,19 @@ welcomeIntent.matches(/^Hello|Hi|Hey|Hola/i, [
     	if(session.userData.name) {
     		next();
     	} else {
-    		builder.Prompts.text(session, '¡Hola! ¿Cuál es tu nombre?');
+//             morning/dawn   0:00  5:00                  
+// early morning  5:00  6:00                  Good morning
+// morning        6:00  9:00 breakfast        Good morning
+// mid-morning    9:00 11:59 elevenses/       Good morning
+//                           morning tea/
+//                           brunch
+// noon          12:00 12:00 -
+// afternoon     12:00 17:00 lunch/           Good afternoon
+//                           afternoon tea
+// evening       17:00 21:00 supper           Good evening
+// night         21:00 23:00 night-time snack Good evening
+// midnight      23:00  1:00 midnight snack   Good night
+    		builder.Prompts.text(session, greeting + '! ¿Cuál es tu nombre?');
     	}
     },
     function (session, results) {
@@ -49,7 +72,8 @@ welcomeIntent.matches(/^Hello|Hi|Hey|Hola/i, [
             session.send('Mucho gusto %s.', session.userData.name); 
             session.beginDialog('/presention');
         } else {
-            session.send('Bienvenido nuevamente %s.', session.userData.name);
+            //Buenos días/Buenas tardes/ Buenas noches
+            session.send(greeting + '%s!', session.userData.name);
             session.beginDialog('/menu');
         }       
     }
@@ -85,7 +109,7 @@ presentationIntent.onBegin(
 bot.dialog('/menu', [
 
     function (session) {
-        session.send('¿Cómo te puedo ayudar hoy?');
+        session.send('¿Cómo te puedo ayudar?'); //hoy
         builder.Prompts.choice(session,'Interés:', [
             "Noticias",
             "Lotería" ,
@@ -110,17 +134,19 @@ bot.dialog('/menu', [
             //Latest
             cfrApi.fetchLatest(function(result){
                 session.send(result.title + '\n' + result.url);
-                //resultComplete = result;            
+                //resultComplete = result;   
+                 session.beginDialog('/segue');         
             });
        } else if (results.response.entity === "Más Vistas")  {
             //Trending
             cfrApi.fetchTrending(function(result){
                 session.send(result.title + '\n' + result.url); 
-                //resultComplete = result;    
+                //resultComplete = result; 
+                 session.beginDialog('/segue');   
             });
         } 
         //TODO Handle Async Calls
-        session.beginDialog('/segue');
+        //session.beginDialog('/segue');
            
     }
 ]);
