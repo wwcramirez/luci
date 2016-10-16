43e4fff7c8815e09314b3b706dd0f607ff6ccd39
wwcramirez <cramirez@wovenware.com>
2016-10-15 22:53:42 -0400

    Api prototype

---
+++ cfrApi.js
@@ -0,0 +1,45 @@
+var request = require('request');
+
+var apiKey = '449a24ecf4be482da238d3d38563b4a5';
+var endi = 'http://www.elnuevodia.com';
+
+module.exports = {
+	fetchLatest: fetchLatest,
+	fetchTrending: fetchTrending	
+};
+
+function fetchLatest(cb) {
+	request(
+	  { 
+	  	url: 'https://gfrmservices.azure-api.net/end/v3/latest?limit=0', 
+	  	headers: {
+	  		'Ocp-Apim-Subscription-Key': apiKey
+	  	} 
+	  }, function (error, response, body) {
+  		var bodyObj = JSON.parse(body);
+  		var result = {
+  			url: endi + bodyObj.latest[0].url,
+  			title: bodyObj.latest[0].title
+  		};
+
+  		cb(result);
+	  });
+}
+
+function fetchTrending(cb) {
+	request(
+	  { 
+	  	url: 'https://gfrmservices.azure-api.net/end/v3/news/trending', 
+	  	headers: {
+	  		'Ocp-Apim-Subscription-Key': apiKey
+	  	} 
+	  }, function (error, response, body) {
+  		var bodyObj = JSON.parse(body);
+  		var result = {
+  			url: bodyObj.trending_articles.values[0].url,
+  			title: bodyObj.trending_articles.values[0].text
+  		};
+
+  		cb(result);
+	  });
+}
